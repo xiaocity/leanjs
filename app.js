42e1e22ba5e5e889ddba9b52bf3f886ca1c02ae3
xiaocity <cityguo@qq.com>
2017-03-13 19:57:57 +0800

    add some thing

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
 
+var session = require('express-session');
+var flash = require('connect-flash');
+var config = require('config-lite');
+//var routes = require('./routes');
+var pkg = require('./package');
+
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
+var signupRouter =  require('./routes/signup');
+var signinRouter =  require('./routes/signin');
+var signoutRouter =  require('./routes/signout');
+var postsRouter =  require('./routes/posts');
+
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
 
@@ -16,6 +29,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+// session 中间件
+app.use(session({
+  name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
+  secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true,// 强制更新 session
+  saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
+  cookie: {
+    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+  }
+}));
+
+// flash 中间件，用来显示通知
+app.use(flash());
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
+  keepExtensions: true// 保留后缀
+}));
 
 // 设置默认超时时间
 app.use(timeout('15s'));
@@ -31,9 +64,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  res.locals.success = req.flash('success').toString();
+  res.locals.error = req.flash('error').toString();
+  next();
+});
+
+// 路由
+//routes(app);
+
 app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() });
 });
+
+//app.use('/', indexRouter);
+app.use('/users', userRouter);
+app.use('/posts', postsRouter);
+app.use('/signup', signupRouter);
+app.use('/signout', signoutRouter);
+app.use('/signin', signinRouter);
+
+
+
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', require('./routes/todos'));
